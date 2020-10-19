b4bc1a6884b869b15a10a73d9eaac8efa85c08da
Amit Kumar <amitnitsri25@gmail.com>
2020-10-19 21:31:08 +0530

    Master : Welcome To Snake & Ladder Game

---
+++ src/com/Capgemini/SnakeLadderGame/SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.Capgemini.SnakeLadderGame;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  	  	System.out.println("Welcome to this Snake & Ladder Game");
+	}
+
+}
