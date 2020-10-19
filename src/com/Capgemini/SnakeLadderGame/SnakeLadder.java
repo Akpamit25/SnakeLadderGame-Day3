6770c50f19e4ebcbe3b7a9a92ccf30b204ac3b16
Amit Kumar <amitnitsri25@gmail.com>
2020-10-19 23:45:26 +0530

    UC6 : Reporting the number of times, the dice was rolled for winning & also return the position after every roll of die

--- src/com/Capgemini/SnakeLadderGame/SnakeLadder.java
+++ src/com/Capgemini/SnakeLadderGame/SnakeLadder.java
@@ -10,12 +10,13 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int count =0;
 		SnakeLadder sl = new SnakeLadder();
 		System.out.println("Welcome to this Snake & Ladder Game");
 		System.out.println("Let's Roll The Dice");
 		while (playerPosition <= 100) {
+			count++;
 			numberOnDice = (int) (Math.random() * 7) + 1;
-			;
 			System.out.println("Number On Dice After Rolling :" + numberOnDice);
 			sl.checkOption();
 			if (playerPosition < 0) {
@@ -24,8 +25,10 @@ public class SnakeLadder {
 			if (playerPosition > 100) {
 				playerPosition -= numberOnDice;
 			}
+			System.out.println("Player Position after rolling the dice " +count+ " No. of times is " +playerPosition);
 
 		}
+		System.out.println("No. Of Times The Dice Was Rolled For Winning : "+count);
 	}
 
 	public void checkOption() {
