9c90c45b9abc29d50d632477877e9cc4c75a59a6
Amit Kumar <amitnitsri25@gmail.com>
2020-10-19 22:23:26 +0530

    UC3 : Player checks for the option whether there is No Play, Ladder Or Snake

--- src/com/Capgemini/SnakeLadderGame/SnakeLadder.java
+++ src/com/Capgemini/SnakeLadderGame/SnakeLadder.java
@@ -2,13 +2,40 @@ package com.Capgemini.SnakeLadderGame;
 
 public class SnakeLadder {
 
+	public static final int No_game=0;
+  	public static final int Ladder=1;
+  	public static final int Snake=2;
+	 public static int playerPosition = 0;
+	 public static int numberOnDice = 0;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		SnakeLadder sl = new SnakeLadder();
   	  	System.out.println("Welcome to this Snake & Ladder Game");
-  	  	int playerPsoition = 0;
   	  	System.out.println("Let's Roll The Dice");
-  	  	int numberOnDice = (int)(Math.random()*7);
+  	  	numberOnDice = (int)(Math.random()*7);
   	  	System.out.println("Number On Dice After Rolling :"+numberOnDice);
+  	  	sl.checkOption();
+	}
+	
+	public void checkOption()
+	{
+  	  	int option = (int)(Math.random()*3);
+
+  	  	switch(option)
+  	  	{
+  	  	case No_game :
+  	  		break;
+  	  		
+  	  	case Ladder :
+  	  		playerPosition+=numberOnDice;
+  	  		break;
+  	  		
+  	  	case Snake :
+  	  		playerPosition-=numberOnDice;
+  	  		break;
+  	  		
+  	  	}
 	}
 
 }
